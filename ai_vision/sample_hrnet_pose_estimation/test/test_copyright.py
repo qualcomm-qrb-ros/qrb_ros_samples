edb9fa64aacfcd721f291ff54c3baece4cd6746b
guohmiao <guohmiao@qti.qualcomm.com>
2025-08-01 16:19:58 +0800

    fix: add copyright notice
    Signed-off-by: guohmiao <guohmiao@qti.qualcomm.com>

--- ai_vision/sample_hrnet_pose_estimation/test/test_copyright.py
+++ ai_vision/sample_hrnet_pose_estimation/test/test_copyright.py
@@ -1,3 +1,6 @@
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+# SPDX-License-Identifier: BSD-3-Clause-Clear
+
 # Copyright 2015 Open Source Robotics Foundation, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
