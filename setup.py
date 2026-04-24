33f9cec705d72d0d48001229cee0a1491ee95e6d
Fulan Liu <fulaliu@qti.qualcomm.com>
2026-04-24 14:54:40 +0800

    fix: modify model label file
    The original model link is no longer valid and has been replaced with a new link.
    
    Signed-off-by: Fulan Liu <fulaliu@qti.qualcomm.com>

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ package_name = 'sample_resnet101'
 
 setup(
     name=package_name,
-    version='1.0.1.1',
+    version='1.0.2',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/' + package_name,
