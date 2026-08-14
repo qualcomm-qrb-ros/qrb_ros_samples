dbf530e26e9ed496ae28e2133a352e8662af7f67
Tim Zhang <timzhang@qti.qualcomm.com>
2026-08-14 14:13:17 +0800

    feat: add IQ10 hand detection camera launch

---
+++ launch/launch_with_qrb_ros_camera_iq10.py
@@ -0,0 +1,122 @@
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+# SPDX-License-Identifier: BSD-3-Clause-Clear
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import ComposableNodeContainer, Node
+from launch_ros.descriptions import ComposableNode
+
+
+def generate_launch_description():
+    model_path_arg = DeclareLaunchArgument(
+        "model_path",
+        default_value="/opt/model/",
+        description="Path to the hand-detection model files",
+    )
+    model_path = LaunchConfiguration("model_path")
+
+    camera_info_config_file_path = os.path.join(
+        get_package_share_directory("qrb_ros_camera"),
+        "config",
+        "camera_info_OX03F10_yuv.yaml",
+    )
+
+    camera_node_params = {
+        "camera_id": 5,
+        "stream_size": 1,
+        "stream_name": ["stream1"],
+        "stream1": {
+            "height": 720,
+            "width": 1280,
+            "fps": 30,
+        },
+        "camera_info_path": camera_info_config_file_path,
+    }
+
+    qrb_ros_camera_container = ComposableNodeContainer(
+        name="camera_container",
+        namespace="",
+        package="rclcpp_components",
+        executable="component_container",
+        composable_node_descriptions=[
+            ComposableNode(
+                package="qrb_ros_camera",
+                plugin="qrb_ros::camera::CameraNode",
+                name="camera_node",
+                parameters=[camera_node_params],
+                remappings=[
+                    ("/camera_info", "/cam5_stream1_camera_info"),
+                    ("/cam5_stream1", "/image_raw"),
+                ],
+            ),
+        ],
+        output="screen",
+    )
+
+    nn_inference_node_palm_detector = ComposableNode(
+        package="qrb_ros_nn_inference",
+        plugin="qrb_ros::nn_inference::QrbRosInferenceNode",
+        name="nn_inference_node_palm_detector",
+        parameters=[
+            {
+                "backend_option": "/usr/lib/libQnnHtp.so",
+                "model_path": PathJoinSubstitution(
+                    [model_path, "MediaPipeHandDetector.bin"]
+                ),
+            }
+        ],
+        remappings=[
+            ("/qrb_inference_input_tensor", "/palm_detector_input_tensor"),
+            ("/qrb_inference_output_tensor", "/palm_detector_output_tensor"),
+        ],
+    )
+
+    nn_inference_node_landmark_detector = ComposableNode(
+        package="qrb_ros_nn_inference",
+        plugin="qrb_ros::nn_inference::QrbRosInferenceNode",
+        name="nn_inference_node_landmark_detector",
+        parameters=[
+            {
+                "backend_option": "/usr/lib/libQnnHtp.so",
+                "model_path": PathJoinSubstitution(
+                    [model_path, "MediaPipeHandLandmarkDetector.bin"]
+                ),
+            }
+        ],
+        remappings=[
+            ("/qrb_inference_input_tensor", "/landmark_detector_input_tensor"),
+            ("/qrb_inference_output_tensor", "/landmark_detector_output_tensor"),
+        ],
+    )
+
+    nn_inference_container = ComposableNodeContainer(
+        name="image_processing_container",
+        namespace="",
+        package="rclcpp_components",
+        executable="component_container",
+        output="screen",
+        composable_node_descriptions=[
+            nn_inference_node_palm_detector,
+            nn_inference_node_landmark_detector,
+        ],
+    )
+
+    hand_detector_node = Node(
+        package="sample_hand_detection",
+        executable="qrb_ros_hand_detector",
+        output="screen",
+        parameters=[{"model_path": model_path}],
+    )
+
+    return LaunchDescription(
+        [
+            model_path_arg,
+            qrb_ros_camera_container,
+            nn_inference_container,
+            hand_detector_node,
+        ]
+    )
